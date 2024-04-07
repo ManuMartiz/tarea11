31c486b8c8b092b037a6d76b575f09f04522b17c
ManuMartiz <160605447+ManuMartiz@users.noreply.github.com>
2024-04-07 12:04:23 -0400

    Update settings.py

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', '5432']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 # Application definition
 
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': environ.get('SUPABASE_USER'),
         'PASSWORD': environ.get('SUPABASE_PASSWORD'),
-        'HOST': environ.get('SUPABASE_HOST'),
+        'HOST': '127.0.0.1',
         'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',
