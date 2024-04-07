7609dc5c74cf4e3d4fe7207a8b677ed131110c92
ManuMartiz <160605447+ManuMartiz@users.noreply.github.com>
2024-04-07 14:23:11 -0400

    Update settings.py

--- python_heroku_task/settings.py
+++ python_heroku_task/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': environ.get('SUPABASE_USER'),
+        'USER': 'postgres',
         'PASSWORD': environ.get('SUPABASE_PASSWORD'),
         'HOST': environ.get('SUPABASE_HOST'),
         'PORT': '5432',
